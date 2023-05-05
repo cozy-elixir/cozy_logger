0848852a6d9542be74445b07cca36dc93375e438
c4710n <c4710n@users.noreply.github.com>
2023-05-05 15:54:33 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyLogger.MixProject do
   use Mix.Project
 
-  @version "0.3.1"
+  @version "0.4.0"
   @description "Logging helpers, providing various formatters and seamless integrations with other libraries."
   @source_url "https://github.com/cozy-elixir/cozy_logger"
 
