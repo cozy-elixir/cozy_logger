c44d3c3e7ea7608b8b3548fbef2f369f0246d202
c4710n <c4710n@users.noreply.github.com>
2023-05-05 13:16:58 +0800

    chore: apply config only for test env

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,12 @@
 import Config
 
-# use Elixir built-in console backend
-config :logger, backends: [:console]
+if Mix.env() == :test do
+  # use Elixir built-in console backend
+  config :logger, backends: [:console]
 
-# use log formatter provided by CozyLogger
-config :logger, :console,
-  format: {CozyLogger.JsonFormatter, :format},
-  metadata: :all,
-  colors: [enabled: false]
+  # use log formatter provided by CozyLogger
+  config :logger, :console,
+    format: {CozyLogger.JsonFormatter, :format},
+    metadata: :all,
+    colors: [enabled: false]
+end
