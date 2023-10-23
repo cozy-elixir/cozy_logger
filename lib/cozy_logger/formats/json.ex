0039e0a806446fb1eb5de3713c312d677c073049
c4710n <c4710n@users.noreply.github.com>
2023-10-23 19:47:58 +0800

    chore: add TODO when no longer supporting versions prior to v1.15

--- lib/cozy_logger/formats/json.ex
+++ lib/cozy_logger/formats/json.ex
@@ -46,10 +46,15 @@ defmodule CozyLogger.JSON do
 
   alias Logger.Formatter
 
+  # credo:disable-for-next-line
+  # TODO: remove these types when no longer supporting versions prior to v1.15
   @type date :: {1970..10_000, 1..12, 1..31}
   @type time_ms :: {0..23, 0..59, 0..59, 0..999}
   @type date_time_ms :: {date, time_ms}
 
+  # credo:disable-for-next-line
+  # TODO: replace date_time_ms with Logger.Formatter.date_time_ms() when no longer
+  # supporting versions prior to v1.15
   @spec format(atom, term, date_time_ms, keyword) :: IO.chardata()
   def format(level, message, timestamp, metadata) do
     build_base_attrs(level, message, timestamp)
