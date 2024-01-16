b9a6baa17a567d60e9a3f480fe352ec3c60ea4c1
c4710n <c4710n@users.noreply.github.com>
2024-01-16 18:39:56 +0800

    chore(nix): update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,41 +1,35 @@
 {
-  description = "Elixir development environment";
+  description = "Elixir development environment for library";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = with pkgs; mkShell {
+      in
+      {
+        devShells.default = with pkgs; mkShell {
           packages = [
-            beam.packages.erlangR25.elixir_1_15
+            beam.packages.erlang_26.elixir_1_15
           ];
 
           shellHook = ''
             # limit mix to current project
             mkdir -p .nix-mix
-            mkdir -p .nix-hex
             export MIX_HOME=$PWD/.nix-mix
-            export HEX_HOME=$PWD/.nix-hex
-            export ERL_LIBS=$HEX_HOME/lib/erlang/lib
 
             # rewire executables
             export PATH=$MIX_HOME/bin:$PATH
             export PATH=$MIX_HOME/escripts:$PATH
-            export PATH=$HEX_HOME/bin:$PATH
 
             # limit history to current project
             export ERL_AFLAGS="-kernel shell_history enabled -kernel shell_history_path '\"$PWD/.erlang-history\"'"
           '';
         };
-      });
-    };
+      }
+    );
 }
